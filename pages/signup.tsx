698ace816fc52b69fb2214d917859d2c220196a6
Kitan Garcia <kitangar@gmail.com>
2023-04-05 12:59:44 -0700

    Add sign up page and enable routing to login

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -2,10 +2,11 @@ import type { NextPage } from "next";
 import Image from "next/image";
 import { useEffect, useRef } from "react";
 
-const Login: NextPage = () => {
+import LoginNavbar from "../components/LoginNavbar";
+
+const SignUp: NextPage = () => {
   const usernameRef = useRef<HTMLInputElement>(null);
   const passwordRef = useRef<HTMLInputElement>(null);
-  const rememberMeRef = useRef<HTMLInputElement>(null);
 
   // Focus on name when modal opens
   useEffect(() => {
@@ -16,12 +17,7 @@ const Login: NextPage = () => {
 
   return (
     <div className="h-full flex flex-col">
-      <div className="bg-navbar flex justify-between items-center w-full py-4 px-12">
-        <h2 className="text-lg tracking-[.5em] font-semibold">verse</h2>
-        <button className="bg-black rounded-lg text-white px-8 py-2">
-          Sign Up
-        </button>
-      </div>
+      <LoginNavbar isRegistration={true} />
       <div className="m-auto flex flex-col items-center justify-center w-1/3 h-2/3">
         <Image
           className="relative bottom-10"
@@ -49,26 +45,13 @@ const Login: NextPage = () => {
               placeholder="Enter your password"
             />
           </div>
-          <div>
-            <label className="text-secondary-text cursor-pointer">
-              <input
-                className="mr-2 mt-4 cursor-pointer"
-                type="checkbox"
-                ref={rememberMeRef}
-              />
-              Remember me
-            </label>
-          </div>
         </div>
         <button className="bg-black mt-4 rounded-lg w-1/2 text-white px-8 py-3">
-          Log in
+          Sign up
         </button>
-        <p className="mt-4 text-secondary-text cursor-pointer">
-          Forgot password?
-        </p>
       </div>
     </div>
   );
 };
 
-export default Login;
+export default SignUp;
