891f33983214c6c69dd4060f9d0d498c9e5eb8f1
Kitan Garcia <kitangar@gmail.com>
2023-04-07 18:15:36 -0700

    fix dashboard navbar positioning

--- components/DashboardNavbar.tsx
+++ components/DashboardNavbar.tsx
@@ -6,9 +6,9 @@ interface DashboardNavbarProps {
 
 const DashboardNavbar = ({ imageSrc }: DashboardNavbarProps) => {
   return (
-    <div className="bg-white absolute top-0 h-16 border-b-2 w-full">
+    <div className="bg-white absolute top-0 h-16 border-b-2 w-full right-0">
       <Image
-        className="rounded-full cursor-pointer relative top-1/2 -translate-y-2/4 left-3/4"
+        className="rounded-full cursor-pointer relative top-1/2 -translate-y-2/4 left-9/10"
         src={imageSrc}
         alt={"Verse logo"}
         width={"40"}
