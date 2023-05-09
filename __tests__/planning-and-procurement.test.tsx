833469cad38d97ffffc4ad3dff41f8fc7b870b2e
Kitan Garcia <kitangar@gmail.com>
2023-05-08 21:57:27 -0700

    test presence of sidebar and navbar

--- __tests__/planning-and-procurement.test.tsx
+++ __tests__/planning-and-procurement.test.tsx
@@ -4,14 +4,29 @@ import "@testing-library/jest-dom";
 
 describe("Planning and Procurement", () => {
   it("renders the planning and procurement page with a sidebar, navbar, and 4 tiles", () => {
-    const { getByTestId } = render(<PlanningAndProcurement />);
-
     // Check that the components appear
+    const { getByTestId } = render(<PlanningAndProcurement />);
     expect(getByTestId("tile1")).toBeInTheDocument();
     expect(getByTestId("tile2")).toBeInTheDocument();
     expect(getByTestId("tile3")).toBeInTheDocument();
     expect(getByTestId("tile4")).toBeInTheDocument();
     expect(getByTestId("sidebar")).toBeInTheDocument();
-    expect(getByTestId("dashboardNavbar")).toBeInTheDocument();
+    expect(getByTestId("dashboard-navbar")).toBeInTheDocument();
+  });
+
+  it("renders the sidebar with a title, and 4 options", () => {
+    const { getByTestId } = render(<PlanningAndProcurement />);
+    expect(getByTestId("sidebar")).toBeInTheDocument();
+    expect(getByTestId("home-link")).toBeInTheDocument();
+    expect(getByTestId("planning-and-procurement")).toBeInTheDocument();
+    expect(getByTestId("delivery")).toBeInTheDocument();
+    expect(getByTestId("optimization")).toBeInTheDocument();
+    expect(getByTestId("monitoring-and-reporting")).toBeInTheDocument();
+  });
+
+  it("renders the navbar with a profile picture", () => {
+    const { getByTestId } = render(<PlanningAndProcurement />);
+    expect(getByTestId("dashboard-navbar")).toBeInTheDocument();
+    expect(getByTestId("profile-picture")).toBeInTheDocument();
   });
 });
--- components/DashboardNavbar.tsx
+++ components/DashboardNavbar.tsx
@@ -7,10 +7,11 @@ interface DashboardNavbarProps {
 const DashboardNavbar = ({ imageSrc }: DashboardNavbarProps) => {
   return (
     <div
-      data-testid="dashboardNavbar"
+      data-testid="dashboard-navbar"
       className="bg-white absolute top-0 h-16 border-b-2 w-full right-0"
     >
       <Image
+        data-testid="profile-picture"
         className="rounded-full cursor-pointer relative top-1/2 -translate-y-2/4 left-9/10"
         src={imageSrc}
         alt={"Verse logo"}
